15f7b2ae5d471d848a7213bd00d67a9c2355ac01
Lucas Boos <lucasbooslb@gmail.com>
2025-08-09 23:12:26 -0300

    middleware in measurement routes

--- src/routes/measure.routes.ts
+++ src/routes/measure.routes.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { upload, confirm, list } from '../controllers/measure.controller';
-// import { authMiddleware } from '../middlewares/auth.middleware';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 export default async (fastify: FastifyInstance) => {
-  fastify.post('/upload', upload);
-  fastify.patch('/confirm', confirm);
-  fastify.get('/:customer_code/list', list);
+  fastify.post('/upload', { preHandler: authMiddleware }, upload);
+  fastify.patch('/confirm', { preHandler: authMiddleware }, confirm);
+  fastify.get('/:customer_code/list', { preHandler: authMiddleware }, list);
 };
